57e4aedfa6a28e5d6147df9ae7f38edbb9cb5981
Darlan444 <darlaninacio321@gmail.com>
2024-07-07 01:28:21 -0300

    favicon

--- js/draggable.js
+++ js/draggable.js
@@ -1,2 +1,7 @@
-
 $("#draggable").draggable();
+$("#draggable2").draggable();
+$("#draggable3").draggable();
+$("#draggable4").draggable();
+$("#draggable5").draggable();
+$("#draggable6").draggable();
+$("#draggable7").draggable();
