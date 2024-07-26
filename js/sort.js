331cb882bea2a3e0504553b68b138d00de3129c2
Darlan444 <darlaninacio321@gmail.com>
2024-07-25 22:07:33 -0300

    clear project

--- js/sort.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var container = document.getElementById('container');
-    for (var i = container.children.length; i >= 0; i--) {
-        container.appendChild(container.children[Math.random() * i | 0]);
-    }
-});
