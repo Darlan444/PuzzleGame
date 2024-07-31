407904ab93a419aa67dd5ba6b8f50918eed7d73b
Darlan444 <darlaninacio321@gmail.com>
2024-07-30 23:07:16 -0300

    dragLeave correction

--- js/script.js
+++ js/script.js
@@ -102,18 +102,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //map elements position on the table
     const placeholdersMapping = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-        51, 52, 53, 54, 55, 56, 57, 72, 73, 74,
-        75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
-        85, 86, 87, 88, 89, 104, 105, 106, 107, 108,
-        109, 110, 111, 112, 113, 114, 115, 116, 117, 118,
-        58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
-        68, 69, 70, 71, 90, 91, 92, 93, 94, 95,
-        96, 97, 98, 99, 100, 101, 102, 103
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+        13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 
+        23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 
+        33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 
+        43, 44, 45, 46, 47, 48, 49, 50, 51, 52,
+        53, 54, 55, 56, 57, 72, 73, 74, 75, 76,
+        77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+        87, 88, 89, 104, 105, 106, 107, 108, 109,
+        110, 111, 112, 113, 114, 115, 116, 117,
+        118, 58, 59, 60, 61, 62, 63, 64, 65, 66,
+        67, 68, 69, 70, 71, 90, 91, 92, 93, 94,
+        95, 96, 97, 98, 99, 100, 101, 102, 103
     ];
 
     placeholdersArray.forEach((placeholder, index) => {
@@ -172,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function () {
             if (this.dataset.number == draggedElement.dataset.number) {
                 this.classList.add('correct');
                 this.classList.remove('incorrect');
-            } else {
+            } else{
                 this.classList.add('incorrect');
                 this.classList.remove('correct');
             }
@@ -196,6 +196,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     });
                 }
             }
+            
         });
     });
     // Observer Configuration - watch additions to the DOM
